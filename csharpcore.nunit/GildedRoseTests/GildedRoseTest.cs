3a47b46ea021057ae9a412231d42043819614f3b
Philippe Villeneuve <Philippe.Villeneuve@primerogames.com>
2023-10-10 16:00:02 -0400

    Initial Import nUnit

---
+++ csharpcore.nunit/GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        GildedRose app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That("fixme", Is.EqualTo(items[0].Name));
+    }
+}
