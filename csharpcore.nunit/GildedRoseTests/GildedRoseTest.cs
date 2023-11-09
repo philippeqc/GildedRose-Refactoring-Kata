c3534895e36efa635a7f34542a2c98a7a0a68540
Philippe Villeneuve <Philippe.Villeneuve@primerogames.com>
2023-11-09 15:23:54 -0500

    QualityOfItemIsNeverNegative

--- csharpcore.nunit/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore.nunit/GildedRoseTests/GildedRoseTest.cs
@@ -9,15 +9,77 @@ public class GildedRoseTest
     [SetUp]
     public void Setup()
     {
-        
+
     }
 
     [Test]
-    public void Test1()
+    public void SellInDecreaseByOneEachDay()
     {
-        IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        // Arrange
+        var sellIn = 10;
+        var expectedSellIn = sellIn - 1;
+        IList<Item> items = GetSampleItem(sellIn: sellIn);
         GildedRose app = new GildedRose(items);
+
+        // Act
         app.UpdateQuality();
-        Assert.That("fixme", Is.EqualTo(items[0].Name));
+
+        // Assert
+        Assert.That(items[0].SellIn, Is.EqualTo(expectedSellIn));
+    }
+
+    [Test]
+    public void QualityDecreaseByOneEachDay()
+    {
+        // Arrange
+        var quality = 10;
+        var expectedQuality = quality - 1;
+        IList<Item> items = GetSampleItem(quality: quality);
+        GildedRose app = new GildedRose(items);
+
+        // Act
+        app.UpdateQuality();
+
+        // Assert
+        Assert.That(items[0].Quality, Is.EqualTo(expectedQuality));
+    }
+
+
+    [Test]
+    public void OnceSellInDatePassedQualityDecreaseTwiceAsFast()
+    {
+        // Arrange
+        var quality = 10;
+        var expectedQuality = quality - 2;
+        IList<Item> items = GetSampleItem(sellIn: 0, quality: quality);
+        GildedRose app = new GildedRose(items);
+
+        // Act
+        app.UpdateQuality();
+
+        // Assert
+        Assert.That(items[0].Quality, Is.EqualTo(expectedQuality));
+    }
+
+    [Test]
+    public void QualityOfItemIsNeverNegative()
+    {
+        // Arrange
+        var quality = 0;
+        var expectedQuality = 0;
+        IList<Item> items = GetSampleItem(quality: quality);
+
+        GildedRose app = new GildedRose(items);
+
+        // Act
+        app.UpdateQuality();
+
+        // Assert
+        Assert.That(items[0].Quality, Is.EqualTo(expectedQuality));
+    }
+
+    public IList<Item>  GetSampleItem(string name = "foo", int sellIn = 10, int quality = 10) {
+        IList<Item> items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
+        return items;
     }
 }
