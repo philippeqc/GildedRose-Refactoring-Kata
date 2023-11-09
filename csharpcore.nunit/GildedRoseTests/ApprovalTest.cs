19be65fea5211a95fcd8d3814f5d26ef7e11b044
Philippe Villeneuve <Philippe.Villeneuve@primerogames.com>
2023-11-09 17:05:37 -0500

    Quick examples of approval tests
    Duplicated first 3 from GildedRoseTest

--- csharpcore.nunit/GildedRoseTests/ApprovalTest.cs
+++ csharpcore.nunit/GildedRoseTests/ApprovalTest.cs
@@ -1,9 +1,7 @@
-using System;
-using System.IO;
 using System.Text;
 using ApprovalTests;
 using ApprovalTests.Reporters;
-using NUnit.Framework;
+using GildedRoseKata;
 
 namespace GildedRoseTests;
 
@@ -23,4 +21,63 @@ public class ApprovalTest
 
         Approvals.Verify(output);
     }
+
+    [Test]
+    public void SellInDecreaseByOneEachDay()
+    {
+        // Arrange
+        var sellIn = 10;
+        (GildedRose app, Item item) = Helper.GetAppWithSampleItem(sellIn: sellIn);
+
+        // Act
+        app.UpdateQuality();
+
+        // Assert
+        Approvals.Verify(item);
+    }
+
+    // [Test]
+    // public void QualityDecreaseByOneEachDay()
+    // {
+    //     // Arrange
+    //     var quality = 10;
+    //     (GildedRose app, Item item) = Helper.GetAppWithSampleItem(quality: quality);
+
+    //     // Act
+    //     app.UpdateQuality();
+
+    //     // Assert
+    //     Approvals.Verify(item);
+    // }
+
+    // [Test]
+    // public void OnceSellInDatePassedQualityDecreaseTwiceAsFast()
+    // {
+    //     // Arrange
+    //     var quality = 10;
+    //     (GildedRose app, Item item) = Helper.GetAppWithSampleItem(sellIn: 0, quality: quality);
+
+    //     // Act
+    //     app.UpdateQuality();
+
+    //     // Assert
+    //     Approvals.Verify(item);
+    // }
+
+
+    // [Test]
+    // public void QualityOfItemIsNeverNegative()
+    // {
+    //     // !!!
+    //     Assert.Fail();
+    // }
+
+
+    // [Test]
+    // public void AgedBrieRaiseQualityWithTime()
+    // {
+    //     // !!!
+    //     Assert.Fail();
+    // }
+
 }
